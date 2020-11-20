53cf2d508662f2d78df13cfa5c11da86e1fd3663
Jeremy Winkler <Jeremy.Winkler@D2L.com>
2020-11-20 11:53:13 -0500

    Accessability changes

--- d2l-step.js
+++ d2l-step.js
@@ -21,7 +21,32 @@ class D2LStep extends LocalizeMixin(LitElement) {
 			disableNextButton: {
 				type: Boolean,
 				attribute: 'disable-next-button'
+			},
+			nextButtonAriaLabel: {
+				type: String,
+				attribute: 'next-button-aria-label'
+			},
+			restartButtonAriaLabel: {
+				type: String,
+				attribute: 'restart-button-aria-label'
+			},
+			ariaTitle:{
+				type: String,
+				attribute: 'aria-title'
+			},
+			title: {
+				type: String,
+				attribute: 'title'
+			},
+			stepCount: {
+				type: Number,
+				attribute: 'step-count'
+			},
+			thisStep: {
+				type: Number,
+				attribute: 'this-step'
 			}
+
 		};
 	}
 
@@ -36,6 +61,15 @@ class D2LStep extends LocalizeMixin(LitElement) {
 			.button-next {
 				float: right;
 			}
+
+			.screen-reader-only {
+				position:absolute;
+				left:-10000px;
+				top:auto;
+				width:1px;
+				height:1px;
+				overflow:hidden;
+			}
 		`;
 	}
 
@@ -47,17 +81,34 @@ class D2LStep extends LocalizeMixin(LitElement) {
 		super();
 		this.hideRestartButton = false;
 		this.disableNextButton = false;
+		this.nextButtonAriaLabel = '';
+		this.restartButtonAriaLabel = '';
+		this.ariaTitle = '';
 	}
 
 	render() {
 		return html`
+			<div id="aria-title" tabindex="0" class="screen-reader-only">${this._getAriaTitle() }</div>
 			<slot></slot>
 			<div class="footer">
-				${this.hideRestartButton ? html`<div></div>` : html`<d2l-button @click="${this._restartClick}">${!this.restartButtonTitle ? this.localize('stepper.defaults.restart') : this.restartButtonTitle}</d2l-button>`}
+				${this.hideRestartButton ? html`<div></div>` : html`<d2l-button aria-label="${this.restartButtonAriaLabel}" @click="${this._restartClick}">${!this.restartButtonTitle ? this.localize('stepper.defaults.restart') : this.restartButtonTitle}</d2l-button>`}
 
-				<d2l-button class="button-next" @click="${this._nextClick}" primary ?disabled="${this.disableNextButton}">${!this.nextButtonTitle ? this.localize('stepper.defaults.next') : this.nextButtonTitle}</d2l-button>
+				<d2l-button class="button-next" aria-label="${this.nextButtonAriaLabel}" @click="${this._nextClick}" primary ?disabled="${this.disableNextButton}">${!this.nextButtonTitle ? this.localize('stepper.defaults.next') : this.nextButtonTitle}</d2l-button>
 			</div>
 		`;
+	}
+
+	_getAriaTitle() {
+		if (this.ariaTitle) {
+			return this.ariaTitle;
+		} else if (this.title) {
+			return `${this.title}. ${this.localize('aria.steplabel', 'totalSteps', this.stepCount, 'currentStep', this.thisStep)}`;
+		}
+		return this.localize('aria.steplabel', 'totalSteps', this.stepCount, 'currentStep', this.thisStep);
+	}
+
+	_getStepLabel() {
+		return this.localize('aria.steplabel', 'totalSteps', this.stepCount, 'currentStep', this.thisStep);
 	}
 
 	_nextClick() {

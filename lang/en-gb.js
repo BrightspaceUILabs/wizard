e08004dee8d701ac73fc3802c67b8d6be72be588
tie.d2l.dev <TieUser@d2l.com>
2024-01-22 16:11:19 +0000

    Translation Updates

---
+++ lang/en-gb.js
@@ -0,0 +1,9 @@
+/* eslint quotes: 0 */
+
+export default {
+	"aria.steplabel": "Step {currentStep} of {totalSteps}",
+	"stepper.defaults.next": "Next",
+	"stepper.defaults.restart": "Restart",
+	"restart.button.tooltip": "Back to first step",
+	"next.button.tooltip": "Proceed to next step"
+};
